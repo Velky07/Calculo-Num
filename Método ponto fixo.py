b214442310637ace25a045589dc477c01c0a4c3e
Velky07 <jvvilela_pe@hotmail.com>
2022-12-13 13:07:05 -0300

    Ajuste na Função phi

--- Método ponto fixo.py
+++ Método ponto fixo.py
@@ -4,10 +4,10 @@ def f(x):
 	return math.exp(x) - (4 * math.pow(x, 2))
 	
 def phi(x):
-	return math.exp(x)/(4 * math.sqrt(math.exp(x)))
+	return (math.sqrt(math.exp(x))/math.sqrt(4))
 
 #Intervalo Inicial
-x= 4.4
+x= 0
 
 #Contagem de Iterações
 k= 0
@@ -21,7 +21,5 @@ while abs (f(x))>erro:
 	x= phi(x)
 	x_raiz=x
 	print("A solução atual é",x_raiz, "\nA interação atual é:", k,"\n")
-	if k>=30:
-		break
 
 print("A solução final é:",x_raiz,"\nTotal de interações foi:", k)
